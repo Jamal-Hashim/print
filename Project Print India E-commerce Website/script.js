f22d7ca66cfefe1c097b8d48fdfa1dc19aff0b6f
Jamal hashim <jamalhashim9120@gmail.com>
2025-11-05 17:59:09 +0530

    Delete Project Print India E-commerce Website directory

--- Project Print India E-commerce Website/script.js
+++
@@ -1,9 +0,0 @@
-const menuToggle = document.getElementById('menu-toggle');
-  const navLinks = document.getElementById('nav-links');
-
-  menuToggle.addEventListener('click', () => {
-    navLinks.classList.toggle('active');
-
-    // Change icon from ☰ to ✖ dynamically
-    menuToggle.textContent = navLinks.classList.contains('active') ? '✖' : '☰';
-  });
